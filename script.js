d98d6e22acd522da1c8d58ca6c4551f4f2568c27
Amelia <120755144+yh63935@users.noreply.github.com>
2026-07-14 21:37:06 -0700

    Add global variables for DOM elements in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+// Get references to the DOM elements
+const chatbotMessages = document.querySelector(".chatbot-messages");
+const options = document.querySelector(".options");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input button");
+
